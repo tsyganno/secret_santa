8d918e140909ca801802318b94f29781e20b2c00
tsyganno <tsyganlee@gmail.com>
2022-12-06 20:58:19 +0800

    third

--- db.py
+++ db.py
@@ -1,2 +1,57 @@
+import sqlite3
 
+
+class Sql_lite:
+
+    def __init__(self):
+        self.conn = sqlite3.connect('db/database.db', check_same_thread=False)
+        self.cursor = self.conn.cursor()
+
+    def write_to_the_database(self, id_user: int, first_name: str, last_name: str, username: str, date: str, person_name: str):
+        try:
+            self.cursor.execute(
+                'INSERT INTO data (id_user, first_name, last_name, username, date, person_name) VALUES (?, ?, ?, ?, ?, ?)',
+                (
+                    id_user,
+                    first_name,
+                    last_name,
+                    username,
+                    date,
+                    person_name,
+                )
+            )
+            self.conn.commit()
+        except sqlite3.Error as error:
+            print("Ошибка при записи данных в БД SQLite", error)
+
+    # def finding_a_duplicate_entry_in_the_database(self, text: str) -> bool:
+    #     """Поиск повторяющейся записи в БД"""
+    #     if self.flag_db == 'True':
+    #         try:
+    #             sql_select_query = """SELECT * FROM data WHERE text = ?"""
+    #             self.cursor.execute(sql_select_query, (text,))
+    #             records = self.cursor.fetchall()
+    #             if len(records) > 0:
+    #                 return True
+    #             else:
+    #                 return False
+    #         except sqlite3.Error as error:
+    #             print("Ошибка при работе с SQLite", error)
+    #
+    # def deleting_extra_entries(self):
+    #     """Удаление лишних записей, начинается после 1000 шт"""
+    #     if self.flag_db == 'True':
+    #         try:
+    #             while True:
+    #                 sql_select_query = """SELECT * FROM data"""
+    #                 self.cursor.execute(sql_select_query)
+    #                 records = self.cursor.fetchall()
+    #                 if len(records) > 1000:
+    #                     sql_select_query = """DELETE FROM data WHERE id = (SELECT min(id) FROM data)"""
+    #                     self.cursor.execute(sql_select_query)
+    #                     self.conn.commit()
+    #                 else:
+    #                     break
+    #         except sqlite3.Error as error:
+    #             print("Ошибка при работе с SQLite", error)
 
--- secret_santa.py
+++ secret_santa.py
@@ -1,11 +1,12 @@
 import logging
-import sqlite3
+from random import choice
 from os import getenv
 import time
 from aiogram import Bot, Dispatcher, types, executor
 from dotenv import load_dotenv
-load_dotenv()
 
+from db import Sql_lite
+load_dotenv()
 
 API_TOKEN = getenv('TOKEN')
 logging.basicConfig(level=logging.INFO)
@@ -13,10 +14,39 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+db = Sql_lite()
+
+people = ['Моника Белуччи', 'Корней Тарасов', 'Киану Ривз', 'Джейсон Стэтхем', 'Роберт Родригес']
+
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply('lol')
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    button_1 = types.KeyboardButton('/s')
+    keyboard.add(button_1)
+    await message.reply('Привет, я Тайный Санта =)', reply_markup=keyboard)
+
+
+@dp.message_handler(commands=['s'])
+async def echo(message: types.Message):
+    try:
+        us_id = message.from_user.id
+        us_name = message.from_user.first_name
+        us_sname = message.from_user.last_name
+        user_name = message.from_user.username
+        time_user = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        name_of_person = choice(people)
+        db.write_to_the_database(
+            id_user=us_id,
+            first_name=us_name,
+            last_name=us_sname,
+            username=user_name,
+            date=time_user,
+            person_name=name_of_person,
+        )
+        await message.answer(f'Вы выбрали {name_of_person}')
+    except:
+        await message.answer('СОБАКА')
 
 
 if __name__ == '__main__':
