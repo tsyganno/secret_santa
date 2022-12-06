2317d1543a8c85576c1330c362417c0b666ee689
tsyganno <tsyganlee@gmail.com>
2022-12-06 22:11:38 +0800

    third

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ class Sql_lite:
         self.cursor = self.conn.cursor()
 
     def write_to_the_database(self, id_user: int, first_name: str, last_name: str, username: str, date: str, person_name: str):
+        """Запись пользователя в БД"""
         try:
             self.cursor.execute(
                 'INSERT INTO data (id_user, first_name, last_name, username, date, person_name) VALUES (?, ?, ?, ?, ?, ?)',
@@ -24,20 +25,33 @@ class Sql_lite:
         except sqlite3.Error as error:
             print("Ошибка при записи данных в БД SQLite", error)
 
-    # def finding_a_duplicate_entry_in_the_database(self, text: str) -> bool:
-    #     """Поиск повторяющейся записи в БД"""
-    #     if self.flag_db == 'True':
-    #         try:
-    #             sql_select_query = """SELECT * FROM data WHERE text = ?"""
-    #             self.cursor.execute(sql_select_query, (text,))
-    #             records = self.cursor.fetchall()
-    #             if len(records) > 0:
-    #                 return True
-    #             else:
-    #                 return False
-    #         except sqlite3.Error as error:
-    #             print("Ошибка при работе с SQLite", error)
-    #
+    def search_name_in_database(self, person_name: str) -> bool:
+        """Поиск имени человека, которому должны подарить подарок, в БД"""
+        try:
+            sql_select_query = """SELECT * FROM data WHERE person_name = ?"""
+            self.cursor.execute(sql_select_query, (person_name,))
+            records = self.cursor.fetchall()
+            if len(records) > 0:
+                return True
+            else:
+                return False
+        except sqlite3.Error as error:
+            print("Ошибка при работе с SQLite", error)
+
+    def search_id_user_in_database(self, id_user: int):
+        """Определение уникальности пользователя"""
+        try:
+            sql_select_query = """SELECT * FROM data WHERE id_user = ?"""
+            self.cursor.execute(sql_select_query, (id_user,))
+            record = self.cursor.fetchall()
+            if len(record) > 0:
+                return record[-1][-1]
+            else:
+                return False
+        except sqlite3.Error as error:
+            print("Ошибка при работе с SQLite", error)
+
+
     # def deleting_extra_entries(self):
     #     """Удаление лишних записей, начинается после 1000 шт"""
     #     if self.flag_db == 'True':
--- secret_santa.py
+++ secret_santa.py
@@ -19,15 +19,22 @@ db = Sql_lite()
 people = ['Моника Белуччи', 'Корней Тарасов', 'Киану Ривз', 'Джейсон Стэтхем', 'Роберт Родригес']
 
 
+def keyboard_secret_santa():
+    """Создание кнопки HAPPY_NEW_YEAR"""
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    button_1 = types.KeyboardButton('/HAPPY_NEW_YEAR')
+    keyboard.add(button_1)
+    return keyboard
+
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-    button_1 = types.KeyboardButton('/s')
-    keyboard.add(button_1)
-    await message.reply('Привет, я Тайный Санта =)', reply_markup=keyboard)
+    await message.reply('Привет, я Тайный Санта =)\n'
+                        'Для определения человека, которому тебе нужно будет подарить подарок, '
+                        'нажми кнопку "HAPPY_NEW_YEAR"', reply_markup=keyboard_secret_santa())
 
 
-@dp.message_handler(commands=['s'])
+@dp.message_handler(commands=['HAPPY_NEW_YEAR'])
 async def echo(message: types.Message):
     try:
         us_id = message.from_user.id
@@ -35,18 +42,34 @@ async def echo(message: types.Message):
         us_sname = message.from_user.last_name
         user_name = message.from_user.username
         time_user = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        name_of_person = choice(people)
-        db.write_to_the_database(
-            id_user=us_id,
-            first_name=us_name,
-            last_name=us_sname,
-            username=user_name,
-            date=time_user,
-            person_name=name_of_person,
-        )
-        await message.answer(f'Вы выбрали {name_of_person}')
+
+        while True:
+            name_of_person = choice(people)
+            flag_name_person = db.search_name_in_database(name_of_person)
+            if flag_name_person is False:
+                break
+
+        flag_write_in_db = db.search_id_user_in_database(us_id)
+        if flag_write_in_db is False:
+            db.write_to_the_database(
+                id_user=us_id,
+                first_name=us_name,
+                last_name=us_sname,
+                username=user_name,
+                date=time_user,
+                person_name=name_of_person,
+            )
+            await message.answer(f'Вы выбрали {name_of_person}', reply_markup=keyboard_secret_santa())
+        else:
+            await message.answer(f'Вы уже выбрали человека, которому будете дарить подарок.'
+                                 f'\nЕго имя --- {flag_write_in_db}--- =)', reply_markup=keyboard_secret_santa())
     except:
-        await message.answer('СОБАКА')
+        await message.answer('Нажми кнопку "HAPPY_NEW_YEAR" =)', reply_markup=keyboard_secret_santa())
+
+
+@dp.message_handler()
+async def send_welcome(message: types.Message):
+    await message.answer('Нажми кнопку "HAPPY_NEW_YEAR" =)', reply_markup=keyboard_secret_santa())
 
 
 if __name__ == '__main__':
